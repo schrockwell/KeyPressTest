addbd89fb61b040a8cd39429e5acd9a3052fdf01
Rockwell Schrock <schrockwell@gmail.com>
2016-09-12 12:12:38 -0400

    Initial commit

---
+++ KeyPressTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsInput;
+using DirectInput;
+
+namespace KeyPressTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                DirectInput.DirectInput.SendKeyPress(args[0]);
+            }
+        }
+    }
+}
